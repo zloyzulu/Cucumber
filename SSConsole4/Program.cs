fd4607cefcb407f40f8b6e0a9bb6113c3b2e83ab
Stanyslav <stanyslavsky33@mail.ru>
2022-08-18 20:40:39 +0700

    add random

--- SSConsole4/Program.cs
+++ SSConsole4/Program.cs
@@ -1,4 +1,5 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); // 1,2,3,4...9
+
+int numberB = new Random().Next(1, 10);
 int result = numberA + numberB;
 Console.WriteLine(result);
